2f4ad3bca4fadc15525490b1d1089af2674981af
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2024-10-29 15:09:12 +0900

    出力の設定

--- Project/Engine/MyDirectXClass/Pipeline/DXDepthStencil.cpp
+++ Project/Engine/MyDirectXClass/Pipeline/DXDepthStencil.cpp
@@ -27,6 +27,14 @@ void DXDepthStencil::Create(PipelineType pipelineType) {
 		depthStencilDesc_.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
 	}
 
+	if (pipelineType == PipelineType::pParticle) {
+
+		// Depth機能無効
+		depthStencilDesc_.DepthEnable = false;
+		depthStencilDesc_.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO;
+		depthStencilDesc_.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+	}
+
 }
 
 /*////////////////////////////////////////////////////////////////////////////////
